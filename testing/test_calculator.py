2a616a03534d1d76c261b5d9bd625abc38932617
abcc <loveabcc1219@163.com>
2021-02-18 05:09:19 +0800

    测开17期 异常处理

--- testing/test_calculator.py
+++ testing/test_calculator.py
@@ -6,37 +6,51 @@ import yaml
 from pythoncode.Calculator import Calculator
 
 
-def get_datas():
+def get_datas(name, type='int'):
     with open("../datas/calc.yml") as f:
         datas = yaml.safe_load(f)
-        print(datas)
-        return datas['add']['datas'], datas['add']['ids']
+
+    mydata = datas[name][type]['datas']
+    ids = datas[name][type]['ids']
+    return mydata, ids
+
 
 class TestCalc(object):
-    datas,ids = get_datas()
+
+    add_int_data = get_datas("add", "int")
+    print(add_int_data[1])
+    add_float_data = get_datas("add", "float")
+    div_int_data = get_datas("div", "int")
+    div_float_data = get_datas("div", "float")
+    div_0_data = get_datas("div", "div0")
 
     def setup_class(self):
         print("开始计算...")
         self.calc = Calculator()
 
+
     def teardown_class(self):
         print("结束计算！")
 
-    # @pytest.mark.parametrize("a,b, result", [
-    #     [1,1,2],
-    #     [100,200,300],
-    #     [1,0,1]
-    # ])
-    @pytest.mark.parametrize("a,b,result", datas, ids=ids)
-    def test_add(self, a, b, result):
+    @pytest.mark.parametrize("a,b,result", add_int_data[0], ids=add_int_data[1])
+    def test_add_int(self, a, b, result):
         print(f"a={a}, b={b}, result={result}")
         assert  result == self.calc.add(a,b)
 
-    # todo: 相除功能
-    def test_div(self):
-        pass
+    @pytest.mark.parametrize("a,b,result", add_float_data[0], ids=add_float_data[1])
+    def test_add_float(self, a, b, result):
+        print(f"a={a}, b={b}, result={result}")
+        assert result == self.calc.add(a, b)
+
+    @pytest.mark.parametrize("a,b,result", div_int_data[0], ids=div_int_data[1])
+    def test_div(self, a, b, result):
+        assert self.calc.div(a, b) == result
+
+    @pytest.mark.parametrize("a,b,result", div_0_data[0], ids=div_0_data[1])
+    def test_div_0(self, a, b, result):
+        with pytest.raises(ZeroDivisionError):
+            result = a/b
 
 
 if __name__ == '__main__':
-    datas = get_datas()
-    print(datas)
+    pass
